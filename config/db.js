08986d541adabc5517951dc625807056fad9caf2
jitendra giriya <85019904+jitendragiriya@users.noreply.github.com>
2023-06-17 16:42:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const ConnectDb = async () => {
   await mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    .connect(process.env.DB_URL, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true, 
+      useCreateIndex:true, 
+      useFindAndModify:false
     })
     .then(() => console.log("Database connected successfully."))
     .catch((error) => {
