b89897efd1ad0f0f107416497306fa0f14c25e8c
jitendra giriya <jitendragiriya@gmail.com>
2023-08-28 18:13:31 +0530

    strict mode is one in mongodb

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const ConnectDb = async () => { 
+const ConnectDb = async () => {
+  mongoose.set("strictQuery", true);
   await mongoose
     .connect(
       `${process.env.MONGODB_CONNECTION_URL}`,
